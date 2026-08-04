2494663d396458071ccbccafaa175342b7739e1f
JJH728 <72532462+JJH728@users.noreply.github.com>
2026-08-04 12:06:28 +0900

    Update Sound.cs

--- Detection/Sound.cs
+++ Detection/Sound.cs
@@ -5,12 +5,19 @@ namespace Squad
 {
     class Sound
     {
-        private string name;
-        private float radius;
-        private float duration;
-        private SquadBlackBoard.AlertLevel alertLevel;
-        private bool canCrossDimension;
+        public string Name {get;}
+        public float Radius {get;}
+        public float Duration {get;}
+        public SquadBlackboard.AlertLevel AlertLevel {get;}
+        public bool CanCrossDimension {get;}
 
-        public Sound(string name, float 
+        public Sound(string name, float radius, float duration, SqaudBlackboard.AlertLevel alertLevel, bool canCrossDimension)
+        {
+            Name = name;
+            Radius = radius;
+            Duration = duration;
+            AlertLevel = alertLevel;\
+            CanCrossDimension = canCrossDimension;
+        }
     }
 }
