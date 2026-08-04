649bb48b045ca203b36f94556133903da7d9877c
JJH728 <72532462+JJH728@users.noreply.github.com>
2026-08-04 11:30:00 +0900

    Update Sound.cs

--- Detection/Sound.cs
+++ Detection/Sound.cs
@@ -1,8 +1,16 @@
+using UnityEngine;
+using Systems.Collection.Generic;
+
 namespace Squad
 {
     class Sound
     {
-        // 
+        private string name;
+        private float radius;
         private float duration;
+        private SquadBlackBoard.AlertLevel alertLevel;
+        private bool canCrossDimension;
+
+        public Sound(string name, float 
     }
 }
